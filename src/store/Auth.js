ad85b2bdf93f3bbda3210d0436a8ae6f250c39e0
jovialcore <jollydosky@gmail.com>
2021-12-04 17:19:31 +0100

    setting up the login for labdx

--- src/store/Auth.js
+++ src/store/Auth.js
@@ -9,7 +9,19 @@ axios.defaults.baseURL = 'http://127.0.0.1:8000/api/login'
 export default new Vuex.Store({
     state: {
         user: null
-    }
+    },
 
-    
+    mutations: {
+        setUserData() {
+            state.user = userData
+            localStorage.setItem('user', JSON.stringify(userData))
+            axios.defaults.headers.common.Authorization = `Bearer ${userData.token}`
+        },
+
+        clearUserData () {
+            localStorage.removeItem('user')
+            location.reload()
+          }
+
+    }
 })
