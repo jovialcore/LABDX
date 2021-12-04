a621d766709bea36a548bbc05fdc2ac4ab2dacf6
jovialcore <jollydosky@gmail.com>
2021-12-04 23:08:16 +0100

    added the login version of this app

--- src/store/Auth.js
+++ src/store/Auth.js
@@ -7,21 +7,40 @@ Vue.use(Vuex)
 axios.defaults.baseURL = 'http://127.0.0.1:8000/api/login'
 
 export default new Vuex.Store({
-    state: {
+    state: {// state is like data(), the single source of truth
         user: null
     },
 
-    mutations: {
+    mutations: {// mutations are like methods in the case of vuex
         setUserData() {
             state.user = userData
             localStorage.setItem('user', JSON.stringify(userData))
             axios.defaults.headers.common.Authorization = `Bearer ${userData.token}`
         },
 
-        clearUserData () {
+        clearUserData() {
             localStorage.removeItem('user')
             location.reload()
-          }
+        }
 
+    },
+    //actions are used to handle asynchronous operations and actions commit mutations
+    actions: {
+        login({ commit }, credentials) {
+            return axios
+                .post('/login', credentials)
+                .then(({ data }) => {
+                    commit('setUserData', data)
+                })
+        },
+        logout({ commit }) {
+            commit('clearUserData')
+        }
+    },
+
+    getters: {
+        isLogged: state => !!state.user
     }
 })
+
+
