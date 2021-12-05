0708174da2bb04940e227eccdd19724bcdf8b030
jovialcore <jollydosky@gmail.com>
2021-12-05 01:19:36 +0100

    finally understood vuex and i made some notes on this file for remembrance

--- src/store/Auth.js
+++ src/store/Auth.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+import Vuex, { Store } from 'vuex'
 import axios from 'axios'
 
 Vue.use(Vuex)
@@ -12,7 +12,7 @@ export default new Vuex.Store({
     },
 
     mutations: {// mutations are like methods in the case of vuex
-        setUserData() {
+        setUserData(state, userData) {
             state.user = userData
             localStorage.setItem('user', JSON.stringify(userData))
             axios.defaults.headers.common.Authorization = `Bearer ${userData.token}`
@@ -24,9 +24,25 @@ export default new Vuex.Store({
         }
 
     },
+
     //actions are used to handle asynchronous operations and actions commit mutations
     actions: {
         login({ commit }, credentials) {
+
+            // we use this commit  with the reference of argument destructuring in javascript
+            //Ideally, action handlers recieves an arbitary REP Object which can be used in place of the original STORE instance's properties/methods (refer to vue docs: https://vuex.vuejs.org/guide/actions.html#dispatching-actions)
+            /*
+            so if we do something like this:
+
+        login( storeRep ) {
+              storeRep.commit('setUserData)  
+
+        })          
+    the above way of refering of committing a mutation its a situatuion where we don't use argument destructuring
+    for more info on argument destructring check this https://bit.ly/3ErldLo ,
+
+    you can also add other parameters in the function
+    */
             return axios
                 .post('/login', credentials)
                 .then(({ data }) => {
@@ -39,8 +55,12 @@ export default new Vuex.Store({
     },
 
     getters: {
-        isLogged: state => !!state.user
+        isLogged: state => !!state.user // this is a short form to make value to be a boolean; either true or false()
+        //so basically we are checking if the user is logged with true/false
     }
 })
 
-
+//actions commit mutations (caller.commit) but mutations change the state
+//actions can access the state(caller.state) too and getters (caller.getters) 
+//we can call other actions too using caller.dispatch
+//getters are like the computed properties of vues stores
