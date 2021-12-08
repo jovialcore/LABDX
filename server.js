4aa3da9ca188c3baafe5d5d333f57bca8ddd9760
jovialcore <jollydosky@gmail.com>
2021-12-08 17:17:06 +0100

    changed the port from 8080 to 8081

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get(/.*/, function(req, res) {
     res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
-const port = process.env.PORT || 8081
+const port = process.env.PORT || 8080
 app.listen(port)
 console.log(`app is listening on port: ${port}`)
