e284dc6a20a142215ee00dda2140e83c773dba8f
jovialcore <jollydosky@gmail.com>
2021-12-09 10:47:19 +0100

    there should be a fix now

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
+var history = require('connect-history-api-fallback');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 app.use(history());
