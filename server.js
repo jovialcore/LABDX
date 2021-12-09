3476824212984308fa13a9d93e8cc897f952f412
jovialcore <jollydosky@gmail.com>
2021-12-09 10:51:29 +0100

    fixng html 5 history mode issue

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
-var history = require('connect-history-api-fallback');
+const history = require('connect-history-api-fallback');
+app.use(history());
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-app.use(history());
 const port = process.env.PORT || 3000;
 app.listen(port);
