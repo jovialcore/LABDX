728b1ca3b292195cc081c61da796aa4b0f1bce9c
jovialcore <jollydosky@gmail.com>
2021-12-09 10:43:43 +0100

    fixing history mode issue on app refersh

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(history());
 const port = process.env.PORT || 3000;
 app.listen(port);
