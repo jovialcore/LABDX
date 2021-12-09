c47d4d91e512ba2b60aa5f8a903e0a009abf1e4d
jovialcore <jollydosky@gmail.com>
2021-12-09 11:00:01 +0100

    still fixing dtuff from the last request

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 const history = require('connect-history-api-fallback');
-app.use(history());
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(history());
 const port = process.env.PORT || 3000;
 app.listen(port);
