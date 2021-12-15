4dd942b85a129b2bd293bf6909725157c6a710a2
jovialcore <jollydosky@gmail.com>
2021-12-15 13:40:27 +0100

    added the date limitation to only 12 to age 60

--- src/components/pages/register/index.vue
+++ src/components/pages/register/index.vue
@@ -24,6 +24,68 @@
 										<label class="form-control-label">Email Address</label>
 										<input class="form-control" type="email" v-model="email">
 									</div>
+								<div class="row">
+									<div class="form-group">
+										<label>Day</label>
+											<select class="select">
+												<option value="1">1</option>
+												<option value="2">2</option>
+												<option value="3">3</option>
+												<option value="4">4</option>
+												<option value="5">5</option>
+												<option value="6">6</option>
+												<option value="7">7</option>
+												<option value="8">8</option>
+												<option value="9">9</option>
+												<option value="10">10</option>
+												<option value="11">11</option>
+												<option value="12">12</option>
+												<option value="13">13</option>
+												<option value="14">14</option>
+												<option value="15">15</option>
+												<option value="16">16</option>
+												<option value="17">17</option>
+												<option value="18">18</option>
+												<option value="19">19</option>
+												<option value="20">20</option>
+												<option value="21">21</option>
+												<option value="22">22</option>
+												<option value="23">23</option>
+												<option value="24">24</option>
+												<option value="25">25</option>
+												<option value="26">26</option>
+												<option value="27">27</option>
+												<option value="28">28</option>
+												<option value="29">29</option>
+												<option value="30">30</option>
+												<option value="31">31</option>
+											</select>
+									</div>
+								<div class="form-group">
+										<label>Month</label>
+											<select class="select">
+												<option value="1">January</option>
+												<option value="2">Febuary</option>
+												<option value="3">March</option>
+												<option value="4">April</option>
+												<option value="5">May</option>
+												<option value="6">June</option>
+												<option value="7">July</option>
+												<option value="8">August</option>
+												<option value="9">September</option>
+											    <option value="10">October</option>
+												<option value="11">November</option>
+												<option value="12">December</option>
+											</select>
+									</div>
+								<div class="form-group">
+										<label>Year</label>
+											<select class="select">
+												<option v-for="index in 61" :key="index" >  {{2010 - index}}</option>
+												<!-- <option v-for="index in 61" :key='index"value="{{index}}"> {{index}</option> -->
+											</select>
+									</div>
+								</div>
 									<div class="form-group">
 										<label class="form-control-label">Date of birth</label>
 										<input class="form-control" type="date" v-model="dob">
@@ -88,6 +150,7 @@
 				gender: '',
 				cpass: '',
 				pass: '',
+		
 			}
 		},
 
