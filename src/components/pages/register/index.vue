8b58ac8f61563df8a3e05eba879a4acb3f42ed80
jovialcore <jollydosky@gmail.com>
2021-12-15 13:46:31 +0100

    finishing touches on the date stuff

--- src/components/pages/register/index.vue
+++ src/components/pages/register/index.vue
@@ -24,44 +24,14 @@
 										<label class="form-control-label">Email Address</label>
 										<input class="form-control" type="email" v-model="email">
 									</div>
-								<div class="row">
-									<div class="form-group">
+								<div class="row mx-auto">
+									<div class="form-group col">
 										<label>Day</label>
 											<select class="select">
-												<option value="1">1</option>
-												<option value="2">2</option>
-												<option value="3">3</option>
-												<option value="4">4</option>
-												<option value="5">5</option>
-												<option value="6">6</option>
-												<option value="7">7</option>
-												<option value="8">8</option>
-												<option value="9">9</option>
-												<option value="10">10</option>
-												<option value="11">11</option>
-												<option value="12">12</option>
-												<option value="13">13</option>
-												<option value="14">14</option>
-												<option value="15">15</option>
-												<option value="16">16</option>
-												<option value="17">17</option>
-												<option value="18">18</option>
-												<option value="19">19</option>
-												<option value="20">20</option>
-												<option value="21">21</option>
-												<option value="22">22</option>
-												<option value="23">23</option>
-												<option value="24">24</option>
-												<option value="25">25</option>
-												<option value="26">26</option>
-												<option value="27">27</option>
-												<option value="28">28</option>
-												<option value="29">29</option>
-												<option value="30">30</option>
-												<option value="31">31</option>
+												<option v-for="days in 31" v-bind:value ="days" :key="days">{{days}}</option>
 											</select>
 									</div>
-								<div class="form-group">
+								<div class="form-group col">
 										<label>Month</label>
 											<select class="select">
 												<option value="1">January</option>
@@ -78,11 +48,10 @@
 												<option value="12">December</option>
 											</select>
 									</div>
-								<div class="form-group">
+								<div class="form-group col">
 										<label>Year</label>
 											<select class="select">
-												<option v-for="index in 61" :key="index" >  {{2010 - index}}</option>
-												<!-- <option v-for="index in 61" :key='index"value="{{index}}"> {{index}</option> -->
+												<option v-for="years in 61" :key="years" :value="years" >  {{2010 - years}}</option>
 											</select>
 									</div>
 								</div>
